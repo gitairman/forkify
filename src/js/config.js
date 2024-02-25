0158a3603613465cf9b3c931a30cd5c52ef4b940
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-02-25 14:37:35 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+require('dotenv').config();
+
+export const API_URL = process.env.API_URL;
+export const TIMEOUT_SEC = process.env.TIMEOUT_SEC;
+export const RES_PER_PAGE = process.env.RES_PER_PAGE;
+export const KEY = process.env.KEY;
+export const MODAL_CLOSE_SEC = process.env.MODAL_CLOSE_SEC;
